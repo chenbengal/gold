bb85f198968b08d41b0449092d7557728ca3e6c0
chenbengal <Bengal311!>
2021-05-04 19:20:54 +0300

    anomaly based CNN

--- gold_prediction.py
+++ gold_prediction.py
@@ -1,9 +1,8 @@
-import pandas as pd
 from datetime import datetime
 from matplotlib import pyplot as plt
 from modules import *
 import numpy as np
-
+import plotly.graph_objects as go
 
 gold_prices = pd.read_csv('monthly_csv.csv')
 
@@ -77,3 +76,55 @@ plt.show()
 
 gold_prices.describe(include='all')
 
+# Predict
+
+train, valid = gold_prices.iloc[:int(round(len(gold_prices)*0.8,0)),],gold_prices.iloc[int(round(len(gold_prices)*0.8,0)):,]
+
+train.head()
+
+n_factors = 10
+train_ds = CPUDataset(train, n_factors)
+valid_ds = CPUDataset(valid, n_factors)
+
+
+epochs = 50
+cnn_model = CNN(out_size=1)
+dataloaders = {
+    'train': DataLoader(train_ds, batch_size=128, shuffle=True),
+    'valid': DataLoader(valid_ds, batch_size=128)
+}
+optim = opt.Adam(cnn_model.parameters(), lr=1e-1, weight_decay=1e-3)
+sched = opt.lr_scheduler.OneCycleLR(optim, max_lr=1e-3, steps_per_epoch=len(dataloaders['train']), epochs=epochs)
+criterion = nn.MSELoss()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+#if __name__ == "__main__":
+
+losses = train_model(cnn_model, dataloaders, optim, sched, criterion, device, epochs)
+
+plt.plot(range(epochs),losses['train'],label = 'train')
+plt.plot(range(epochs),losses['valid'],label = 'valid')
+plt.legend()
+plt.show()
+
+
+# Switching model into evaluation mode
+cnn_model = cnn_model.eval()
+
+# Calculation of the predictions for training data
+with torch.no_grad():
+    res_train = cnn_model(train_ds[:][0].to(device))
+res_train = res_train.cpu()
+
+# Calculation of the predictions for validation data
+with torch.no_grad():
+    res_valid = cnn_model(valid_ds[:][0].to(device))
+res_valid = res_valid.cpu()
+
+len(res_train)
+
+plt.plot(train.index[10:] ,train['Price'][10:] , label = 'y')
+plt.plot(train.index[10:],res_train,label = 'y_hat')
+plt.legend()
+plt.show()
--- modules.py
+++ modules.py
@@ -1,72 +1,144 @@
-import math
+import pandas as pd
+from tqdm import tqdm
+# import tensorflow as tf
+from torch import optim as opt
+import torch.nn as nn
+import torch
 
-#
-class Welford(object):
-    """ Implements Welford's algorithm for computing a running mean
-    and standard deviation as described at:
-        http://www.johndcook.com/standard_deviation.html
-    can take single values or iterables
-    Properties:
-        mean    - returns the mean
-        std     - returns the std
-        meanfull- returns the mean and std of the mean
-    Usage:
-        >>> foo = Welford()
-        >>> foo(range(100))
-        >>> foo
-        <Welford: 49.5 +- 29.0114919759>
-        >>> foo([1]*1000)
-        >>> foo
-        <Welford: 5.40909090909 +- 16.4437417146>
-        >>> foo.mean
-        5.409090909090906
-        >>> foo.std
-        16.44374171455467
-        >>> foo.meanfull
-        (5.409090909090906, 0.4957974674244838)
-    """
+# PyTorch itself
 
-    def __init__(self, lst=None):
-        self.k = 0
-        self.M = 0
-        self.S = 0
+# Dataset - the base class to be inherited
+from torch.utils.data import Dataset, DataLoader
+# We will need DataLoader later for the training process
 
-        self.__call__(lst)
+class CPUDataset(Dataset):
+    def __init__(self, data: pd.DataFrame, size: int,
+                 step: int = 1):
+        self.chunks = torch.FloatTensor(data['Price']).unfold(0, size + 1, step)
+        self.chunks = self.chunks.view(-1, 1, size + 1)
 
-    def update(self, x):
-        if x is None:
-            return
-        print(self.M)
-        self.k += 1
-        newM = self.M + (x - self.M) * 1. / self.k
-        newS = self.S + (x - self.M) * (x - newM)
-        self.M, self.S = newM, newS
+    def __len__(self):
+        return self.chunks.size(0)
 
-    def consume(self, lst):
-        lst = iter(lst)
-        for x in lst:
-            self.update(x)
+    def __getitem__(self, i):
+        x = self.chunks[i, :, :-1]
+        y = self.chunks[i, :, -1:].squeeze(1)
+        return x, y
 
-    def __call__(self, x):
-        if hasattr(x, "__iter__"):
-            self.consume(x)
-        else:
-            self.update(x)
 
-    @property
-    def mean(self):
-        return self.M
 
-    @property
-    def meanfull(self):
-        return self.mean, self.std / math.sqrt(self.k)
+def conv_layer(in_feat, out_feat, kernel_size=3, stride=1,
+               padding=1, relu=True):
+    res = [
+        nn.Conv1d(in_feat, out_feat, kernel_size=kernel_size,
+                  stride=stride, padding=padding, bias=False),
+        nn.BatchNorm1d(out_feat),
+    ]
+    if relu:
+        res.append(nn.ReLU())
+    return nn.Sequential(*res)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_feat, out_feat):
+        super().__init__()
+        self.in_feat, self.out_feat = in_feat, out_feat
+        self.conv1 = conv_layer(in_feat, out_feat)
+        self.conv2 = conv_layer(out_feat, out_feat, relu=False)
+        if self.apply_shortcut:
+            self.shortcut = conv_layer(in_feat, out_feat,
+                                       kernel_size=1, padding=0,
+                                       relu=False)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        if self.apply_shortcut:
+            x = self.shortcut(x)
+        return x + self.conv2(out)
 
     @property
-    def std(self):
-        if self.k == 1:
-            return 0
-        return math.sqrt(self.S / (self.k - 1))
+    def apply_shortcut(self):
+        return self.in_feat != self.out_feat
 
-    def __repr__(self):
-        return "<Welford: {} +- {}>".format(self.mean, self.std)
 
+class AdaptiveConcatPool1d(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ap = nn.AdaptiveAvgPool1d(1)
+        self.mp = nn.AdaptiveMaxPool1d(1)
+
+    def forward(self, x):
+        return torch.cat([self.mp(x), self.ap(x)], 1)
+
+
+class CNN(nn.Module):
+    def __init__(self, out_size):
+        super().__init__()
+        self.base = nn.Sequential(
+            ResBlock(1, 8),  # shape = batch, 8, n_factors
+            ResBlock(8, 8),
+            ResBlock(8, 16),  # shape = batch, 16, n_factors
+            ResBlock(16, 16),
+            ResBlock(16, 32),  # shape = batch, 32, n_factors
+            ResBlock(32, 32),
+            ResBlock(32, 64),  # shape = batch, 64, n_factors
+            ResBlock(64, 64),
+        )
+        self.head = nn.Sequential(
+            AdaptiveConcatPool1d(),  # shape = batch, 128, 1
+            nn.Flatten(),
+            nn.Linear(128, out_size)
+        )
+
+    def forward(self, x):
+        out = self.base(x)
+        out = self.head(out)
+        return out
+
+
+def train_model(model: CNN, dataloaders: dict, optimizer: opt.Optimizer,
+                scheduler, criterion, device: torch.device, epochs: int):
+    losses_data = {'train': [], 'valid': []}
+    model.to(device)
+
+    # Loop over epochs
+    for epoch in tqdm(range(epochs)):
+        print(f'Epoch {epoch}/{epochs - 1}')
+
+        # Training and validation phases
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.
+            running_total = 0.
+
+            # Loop over batches of data
+            for idx, batch in tqdm(enumerate(dataloaders[phase]),
+                                   total=len(dataloaders[phase]),
+                                   leave=False
+                                   ):
+                x, y = batch
+                x = x.to(device)
+                y = y.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    out = model(x)
+                    loss = criterion(out, y)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                        scheduler.step()
+
+                running_loss += loss.item() * y.size(0)
+                running_total += y.size(0)
+
+            epoch_loss = running_loss / running_total
+            print(f'{phase.capitalize()} Loss: {epoch_loss}')
+            losses_data[phase].append(epoch_loss)
+    return losses_data
